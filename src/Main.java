1a47e4b75ce6711bfac9f97e04b821e2348dcabf
Isaiah Padilla <Ip714@csu.fullerton.edu>
2025-05-27 21:17:00 -0700

    Getting Started.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.awt.Canvas;
+import java.awt.BorderLayout;
+import java.awt.color.*;
+import java.awt.Graphics;
+import java.awt.Window;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+       Window window;
+
+       //window.setSize(500, 500);
+    }
+}
