1b3babefa78141048439278f49fb5ef59d760a3d
Isaiah Padilla <Ip714@csu.fullerton.edu>
2025-05-28 20:04:29 -0700

    Added a window.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
-import java.awt.Canvas;
+/*import java.awt.Canvas;
 import java.awt.BorderLayout;
 import java.awt.color.*;
 import java.awt.Graphics;
-import java.awt.Window;
-
+import java.awt.Window;*/
+import java.awt.*;
+import java.awt.event.*;
 
 public class Main {
+    // Main body of the program
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Frame frame = new Frame("Game Window");
 
-       Window window;
+        frame.setSize(500, 500);
 
-       //window.setSize(500, 500);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+
+        frame.setVisible(true);
     }
 }
