8f0b07f735ad9209e93dbc9d355e9302c86e021f
Isaiah Padilla <Ip714@csu.fullerton.edu>
2025-07-27 22:39:41 -0700

    Drew two rectangles.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,45 @@
-/*import java.awt.Canvas;
-import java.awt.BorderLayout;
-import java.awt.color.*;
-import java.awt.Graphics;
-import java.awt.Window;*/
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.*;    // JFrame
+import javax.swing.*; // JPanel
+
+import javax.swing.JFrame;
+
+// https://introcs.cs.princeton.edu/java/11cheatsheet/
 
 public class Main {
     // Main body of the program
     public static void main(String[] args) throws Exception {
-        Frame frame = new Frame("Game Window");
+        // Instantiated JFrame object to create a window object
+        JFrame frame = new JFrame("Game Window");
 
-        frame.setSize(500, 500);
+        // Setting the size of the window
+        frame.setSize(800, 800);
 
-        frame.addWindowListener(new WindowAdapter() {
+        // Allows the user to exit window by clicking the X icon
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // JPanel class allows object to be drawn onto the window
+        JPanel panel = new JPanel() {
+            // Overriding this method, must use the protected keyword
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                g.setColor(Color.RED);
+                g.fillRect(100, 100, 200, 200);
+
+                repaint();
+
+                g.setColor(Color.GREEN);
+                g.fillRect(200, 600, 100, 100);
+            }
+        };
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+        /*frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
             }
-        });
-
-        frame.setVisible(true);
+        });*/
     }
 }
