b93815acb768731c5f99bb21c99d41c9f30dccf0
Isaiah Padilla <Ip714@csu.fullerton.edu>
2025-08-02 19:17:51 -0700

    Added more classes, I'm tired asf.

---
+++ src/GrassTile.java
@@ -0,0 +1,11 @@
+public class GrassTile extends Tile {
+    GrassTile(Sprite sprite)
+    {
+        super(sprite);
+    }
+
+    public void render(int x, int y, Screen screen)
+    {
+
+    }
+}
---
+++ src/Tile.java
@@ -0,0 +1,21 @@
+public class Tile {
+    public int x, y;
+    public Sprite sprite;
+
+    public static Tile grass = new GrassTile(Sprite.grass);
+
+    public Tile(Sprite sprite)
+    {
+        this.sprite = sprite;
+    }
+
+    public void render(int x, int y, Screen screen)
+    {
+
+    }
+
+    public boolean solid()
+    {
+        return false;
+    }
+}
