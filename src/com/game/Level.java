39c0d013c1e5ab244f6c48e3f596d06645db3943
Isaiah714 <Ip714@csu.fullerton.edu>
2026-07-17 19:10:43 -0700

    Added base class, level assets handled here

--- src/com/game/Level.java
+++ src/com/game/Level.java
@@ -1,5 +1,44 @@
 package com.game;
 
+import java.util.Vector;
+
+enum LevelArea {
+  FLOWERLAND,
+  FORESTLAND,
+  DESERTLAND,
+  TROPICALLAND,
+  ICELAND,
+  BLUEDUNGEON,
+  PURPLEDUNGEON,
+  REDDUNGEON,
+  FINALDUNGEON
+}
+
+class FilePath {
+	public String vertex;
+	public String fragment;
+	public String texture;
+
+	public FilePath(String vertPath, String fragPath, String texPath) {
+		this.vertex = vertPath;
+		this.fragment = fragPath;
+		this.texture = texPath;
+	}
+}
+
 public class Level {
-	
+  private Vector<String> levelAssets;
+  private FilePath path;
+
+  public Level() {
+    this.path = new FilePath("/shaders/tile.vs",
+                                           "/shaders/tile.fs",
+                                           "/textures/tiles.jpg");
+  }
+
+  public Level(Vector<String> levelA) {
+    this.levelAssets = levelA;
+  }
+
+
 }
